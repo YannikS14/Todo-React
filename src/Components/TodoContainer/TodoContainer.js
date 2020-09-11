511d013177f65a5c53cd9bc29ccca1d6ad7a3a80
Yannik Scheibner <yannikscheibner@Yanniks-MBP.fritz.box>
2020-09-11 17:41:33 +0200

    Changed formatting of Todo Component

--- src/Components/TodoContainer/TodoContainer.js
+++ src/Components/TodoContainer/TodoContainer.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './TodoContainer.css';
 import { Todo } from '../Todo/Todo';
+import { uid } from 'react-uid';
 
 export class TodoContainer extends React.Component {
     render() {
         return(
             <div className="todo-container">
                 <ul className="todo-list">
-                    {this.props.todos.map(item => <Todo key={item.id} onDelete={this.props.onDelete} todo={item}/>)}
+                    {this.props.todos.map((item, index) => 
+                    <Todo 
+                    key={uid(item.value, index)} 
+                    onDelete={this.props.onDelete} 
+                    todo={item} 
+                    transitionEnd={this.props.transitionEnd} 
+                    onComplete={this.props.onComplete} 
+                    category={this.props.category}
+                    />)}
                 </ul>
             </div>
         )
