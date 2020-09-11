4f1c4304dc7c3b4b528ae969c6fd2534f7d05bb5
Yannik Scheibner <yannikscheibner@Yanniks-MBP.fritz.box>
2020-09-11 17:41:03 +0200

    Add functions for completing and deleting Todo In Progress: Filter function

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -2,16 +2,38 @@ import React from 'react';
 import './Todo.css';
 
 export class Todo extends React.Component {
+    mountOrUnmount() {
+        if (this.props.todo.status === 'delete') {
+            return this.props.transitionEnd(this.props.todo);
+        }
+    }
+
+    getClassName() {
+        if (this.props.todo.status === 'delete') {
+            return 'todo fall';
+        } else if (this.props.todo.status === 'completed') {
+            return 'todo completed';
+        } else {
+            return 'todo';
+        }
+    }
+
     render() {
+        const content = this.props.category === 'all'
+        ? <div onTransitionEnd={() => this.mountOrUnmount()} className={this.getClassName()}>
+            <li className="todo-item">{this.props.todo.value}</li>
+            <button onClick={() => this.props.onComplete(this.props.todo)} className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={() => this.props.onDelete(this.props.todo)} className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+        : null;
+
         return(
-            <div className="todo">
-                <li className="todo-item">{this.props.todo.todo}</li>
-                <button onClick={this.props.onClick} className="complete-btn">
-                    <i className="fas fa-check"></i>
-                </button>
-                <button onClick={() => this.props.onDelete(this.props.todo.id)} className="trash-btn">
-                    <i className="fas fa-trash"></i>
-                </button>
+            <div>
+            {content}
             </div>
         )
     }
