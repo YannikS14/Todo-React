d193f036ff511d1da558cff361d4de6bb0edbf30
Yannik Scheibner <yannikscheibner@Yanniks-MBP.fritz.box>
2020-09-11 17:38:57 +0200

    Add function for completing and deleting Todo

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import './App.css';
 import { TodoContainer } from '../TodoContainer/TodoContainer';
 import { TodoForm } from '../TodoForm/TodoForm';
+import { uid } from 'react-uid';
 
 export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [
-        {id: 1, todo: 'Petz'},
-        {id: 2, todo: 'Mulm'},
-        {id: 3, todo: 'yee'}
+        {id: 1, value: 'Petz', status: 'active'},
+        {id: 2, value: 'Mulm', status: 'active'},
+        {id: 3, value: 'yeee', status: 'active'}
       ],
-      userInput: ''
+      userInput: '',
+      category: 'all'
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.transitionEnd = this.transitionEnd.bind(this);
+    this.handleComplete = this.handleComplete.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
   }
 
   handleChange(e) {
@@ -27,16 +32,41 @@ export class App extends React.Component {
 
   handleSubmit(e) {
     this.setState({
-      todos: [...this.state.todos, {id: this.state.todos.length+1, todo: this.state.userInput}],
+      todos: [...this.state.todos, {
+        id: uid(this.state.userInput), 
+        value: this.state.userInput, 
+        status: 'active'
+      }],
       userInput: ''
     });
     e.preventDefault();
   }
 
-  handleDelete(todoId) {
-    // To-Do: Add animation when deleting (css class .fall)
-    const todos = this.state.todos.filter(todo => todo.id !== todoId);
+  handleDelete(passedTodo) {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(
+        el => el === passedTodo? { ...el, status: 'delete'}: el
+      )
+    }));
+  }
+
+  transitionEnd(passedTodo) {
+    const todos = this.state.todos.filter(todo => todo !== passedTodo);
     this.setState({ todos: todos });
+  }
+
+  handleComplete(passedTodo) {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(
+        el => el === passedTodo? { ...el, status: 'completed'}: el
+      )
+    }));
+  }
+
+  handleSelect(event) {
+    this.setState({
+      category: event.target.value
+    });
   }
 
   render() {
@@ -45,8 +75,8 @@ export class App extends React.Component {
         <header>
             <h1>React Todo List</h1>
         </header>
-        <TodoForm onChange={this.handleChange} onSubmit={this.handleSubmit} userInput={this.state.userInput}/>
-        <TodoContainer onDelete={this.handleDelete} todos={this.state.todos} />
+        <TodoForm onChange={this.handleChange} onSubmit={this.handleSubmit} userInput={this.state.userInput} handleSelect={this.handleSelect} category={this.state.category}/>
+        <TodoContainer onDelete={this.handleDelete} todos={this.state.todos} transitionEnd={this.transitionEnd} onComplete={this.handleComplete} category={this.state.category}/>
       </div>
     );
   }
