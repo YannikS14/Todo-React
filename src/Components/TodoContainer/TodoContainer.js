0998a15276bff8fb67ca5e67ec25c8a713a778c6
Yannik Scheibner <yannikscheibner@Yanniks-MBP.fritz.box>
2020-09-12 18:08:13 +0200

    Add filter function rendering

--- src/Components/TodoContainer/TodoContainer.js
+++ src/Components/TodoContainer/TodoContainer.js
@@ -5,20 +5,59 @@ import { uid } from 'react-uid';
 
 export class TodoContainer extends React.Component {
     render() {
-        return(
-            <div className="todo-container">
-                <ul className="todo-list">
-                    {this.props.todos.map((item, index) => 
-                    <Todo 
-                    key={uid(item.value, index)} 
-                    onDelete={this.props.onDelete} 
-                    todo={item} 
-                    transitionEnd={this.props.transitionEnd} 
-                    onComplete={this.props.onComplete} 
-                    category={this.props.category}
-                    />)}
-                </ul>
-            </div>
-        )
+        if (this.props.category === 'all') {
+            return(
+                <div className="todo-container">
+                    <ul className="todo-list">
+                        {this.props.todos.map((item, index) => 
+                        <Todo 
+                        key={uid(item.value, index)} 
+                        onDelete={this.props.onDelete} 
+                        todo={item} 
+                        transitionEnd={this.props.transitionEnd} 
+                        onComplete={this.props.onComplete} 
+                        />)}
+                    </ul>
+                </div>
+            )
+        } else if (this.props.category === 'completed') {
+            return(
+                <div className="todo-container">
+                    <ul className="todo-list">
+                        {this.props.todos.map((item, index) =>
+                        item.status === 'completed'?
+                        <Todo 
+                        key={uid(item.value, index)} 
+                        onDelete={this.props.onDelete} 
+                        todo={item} 
+                        transitionEnd={this.props.transitionEnd} 
+                        onComplete={this.props.onComplete} 
+                        />: null)}
+                    </ul>
+                </div>
+            )
+        } else if (this.props.category === 'uncompleted') {
+            return(
+                <div className="todo-container">
+                    <ul className="todo-list">
+                        {this.props.todos.map((item, index) =>
+                        item.status === 'active'?
+                        <Todo 
+                        key={uid(item.value, index)} 
+                        onDelete={this.props.onDelete} 
+                        todo={item} 
+                        transitionEnd={this.props.transitionEnd} 
+                        onComplete={this.props.onComplete} 
+                        />: null)}
+                    </ul>
+                </div>
+            )
+        } else {
+            return(
+                <div className="todo-container">
+                    <ul className="todo-list"></ul>
+                </div>
+            )
+        }
     }
 }
